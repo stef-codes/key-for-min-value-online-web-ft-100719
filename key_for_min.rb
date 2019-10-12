9c809699ad7291a2fab0ba94d066a6dda75a6f8f
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-12 01:04:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
     if min_value == nil || value < min_value 
       min_value = value
       min_key = key 
-    else value 
-  end
+    end
   }
+  return min_key
 end
